56cd27f777b517b50216bff0c26dd98696099675
ikyan17 <ikyan17@gmail.com>
2026-06-19 09:33:23 -0600

    contador de vidas seguimiento de mouse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 
 public class Player : Character
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshProUGUI vidas;
+
     void Start()
     {
-
+        vidas.text += 10;
     }
 
     // Update is called once per frame
